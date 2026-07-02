38b02fc1627b78454ad9fa965f51bd4f5b882b0c
Diego Borba <projetoecam@gmail.com>
2026-07-01 22:57:36 -0300

    Ajuste vercel

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 
@@ -10,9 +11,9 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  // Recebe um único objeto com login e senha
   login(dadosLogin: { login: string, senha: string }): Observable<any> {
-    const url = `${environment.apiUrl}/api/login`;
-    return this.http.post(url, dadosLogin);
+  const url = `${environment.apiUrl}/api/login`;
+  const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  return this.http.post(url, dadosLogin, { headers });
   }
 }
