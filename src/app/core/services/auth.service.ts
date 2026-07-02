603b0f9070b7018388bc6c2a678ad844a40665a5
Diego Borba <projetoecam@gmail.com>
2026-07-01 22:41:11 -0300

    Ajuste Url api

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/api/login';
 
   constructor(private http: HttpClient) { }
 
-  login(login: string, senha: string): Observable<any> {
-    const payload = { login, senha };
-    return this.http.post(this.apiUrl, payload);
+  login(dadosLogin: any) {
+    const url = `${environment.apiUrl}/api/login`;
+    return this.http.post(url, dadosLogin);
   }
 }
